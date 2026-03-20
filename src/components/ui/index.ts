d24b6559081b3f8898cc7fde1bfdf33ca416ab71
NicolasArancibia <nicoarancibia2003@gmail.com>
2026-03-20 15:59:27 -0300

    fix: type imports y export index

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,4 @@
-export { default as OutSystemsUIKit } from './OutSystemsUIKit'  
+export { default as WomButton } from './WomButton'
+export { default as WomCard } from './WomCard'
+export { default as WomInput } from './WomInput'
+export { default as PlanCard } from './PlanCard'
